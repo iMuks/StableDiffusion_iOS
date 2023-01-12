f5c5bb29218abdda48a90fd92f50422820d68faf
Mukesh Kumar sharma <mukesh.ct.pce@gmail.com>
2023-01-12 17:17:36 -0500

    Initial Commit

---
+++ StableDiffusion_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StableDiffusion_iOS
+//
+//  Created by Mukesh Shama on 2023-01-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
